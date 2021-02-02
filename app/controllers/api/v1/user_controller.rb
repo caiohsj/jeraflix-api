1857300667cb07c08280961abba83f253fc7e6fd
Caio Henrique <caiocoxim10@gmail.com>
2021-02-02 07:30:00 -0400

    melhoria de segurança na autenticação de usuário

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,3 +1,4 @@
+require 'base64'
 class Api::V1::UserController < ApplicationController
     def create
         user_exist = User.where email: params[:email]
@@ -18,9 +19,9 @@ class Api::V1::UserController < ApplicationController
     end
 
     def auth
-        
+        password = Base64.decode64(params[:password])
         @user = User.find_for_authentication(email: params[:email])
-        if @user.valid_password?(params[:password])
+        if @user.valid_password?(password)
             render json: {
                 name: @user.name,
                 email: @user.email,
